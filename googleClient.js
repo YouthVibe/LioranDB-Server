488ae3c333c2fc37624bf89886f73fd6be29378b
UltronTheAI <coderswaraj@gmail.com>
2025-11-19 13:21:23 +0530

    Added p2p server

--- googleClient.js
+++ googleClient.js
@@ -1,8 +1,11 @@
 // googleClient.js
 import { OAuth2Client } from 'google-auth-library';
-import fs from 'fs';
+// import fs from 'fs';
+import dotenv from 'dotenv';
 
-const credentials = JSON.parse(fs.readFileSync('./client_secret.json', 'utf8'));
+dotenv.config();
+
+const credentials = JSON.parse(process.env.GOOGLE_SECRET_JSON);
 const { client_id, client_secret, redirect_uris } = credentials.web;
 
 export const oAuth2Client = new OAuth2Client(
